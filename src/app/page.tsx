62a3bffb8a469eab25e6a10f22a8e678aedd178a
gexupph <gexup.ph@gmail.com>
2026-06-24 00:02:31 +0800

    page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Map from "../components/Map";
+
+export default function HomePage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-xl font-bold mb-4">My Map</h1>
+      <Map />
+    </main>
+  );
+}
